a911097a5ae1d8bd398379e4beb546d17558b675
Priyanshu Dwivedi <ankurdwivedi75@gmail.com>
2019-12-17 18:11:41 +0530

    added time package to add epoch time in json data
    Server will receive the data and add the Epoch time to the json data

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
+import time
 app=Flask(__name__)
-alldata=['ankur']
+alldata=[]
 @app.route('/')
 def hello():
 	print ('hello() function called')
@@ -12,7 +13,9 @@ def data():
 		return jsonify(alldata)
 	elif request.method == 'POST':
 		data =request.json
+		data["time"]=time.time()
 		alldata.append(data)
+		print(data)
 		return data
 if __name__=='__main__':
 	app.run(host='0.0.0.0',port=5000)   #host is what type of connection and what should accept , 0.0.0.0 means it can accept every connections
